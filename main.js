ab2b8c79d59752fab3b97162579b8ca1c8584a06
Kissstanley <swyatoslawfyk@gmail.com>
2023-05-01 23:50:00 +0300

    First task js

---
+++ main.js
@@ -0,0 +1,69 @@
+let a = 'hello'
+console.log(a)
+
+let b = 'owu'
+console.log(b)
+
+let c = 'com'
+console.log(c)
+
+let e = 'ua'
+console.log(e)
+
+let f = 1
+console.log(f)
+
+let g = 10
+console.log(g)
+
+let h = -999
+console.log(h)
+
+let i = 123
+console.log(i)
+
+let j = 3.14
+console.log(j)
+
+let k = 2.7
+console.log(k)
+
+let l = 16
+console.log(l)
+
+let m = true
+console.log(m)
+
+let n = false
+console.log(n)
+
+
+
+let x = 'Fyk'
+let y = 'Swyatoslaw'
+let z = 'Vasyliovich'
+
+const result = x + ' ' + y + ' ' +z;
+console.log(result)
+
+
+
+let a1 = 100;
+console.log(typeof a1)
+
+let b1 = '100';
+console.log(typeof b1)
+
+let c1 = true;
+console.log(typeof c1)
+
+
+
+let firstName=prompt('Введіть ваше ім’я')
+let lastName=prompt('Введіть ваше По батькові')
+let age=prompt('Введіть ваш вік')
+
+console.log(`Ім’я: ${firstName} `);
+console.log(`По батькові: ${lastName} `);
+console.log(`Вік: ${age} `);
+
